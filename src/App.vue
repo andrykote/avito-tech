bb27dfb890d168648e72bf22063b60c36b6d916c
Andre Gil <andrykote@gmail.com>
2022-02-18 16:06:55 +0300

    add: 1. add gallery

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modal />
+
+    <footer>
+      <div class="line"></div>
+      <p class="year"> &copy; {{yearNow }}-{{yearNow + 1}} </p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Modal,
+  },
+  data() {
+    return {
+      yearNow: new Date().getFullYear(),
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
+$base-color: #ccc;
+
+* {
+  box-sizing: border-box;
+  text-decoration: none;
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+
+html,
+body {
+  height: 100%;
+  padding: 15px 20px 10px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.line {
+  max-width: 100%;
+  height: 1px;
+  margin-bottom: 10px;
+  background-color: $base-color;
+}
+
+.year {
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $base-color;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="modal-box">
+    <h1>Test App</h1>
+    <div class="gallery">
+      <img
+        class="gallery-item"
+        v-for="item in imgArrLinks"
+        :src="item.url"
+        :key="item.id"
+        alt="img"
+        @click="getArg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      imgArrLinks: [],
+    };
+  },
+
+  methods: {
+    getArg() {
+      console.log(this.imgArrLinks);
+    },
+  },
+
+  mounted() {
+    this.imgArrLinks = fetch(
+      "https://boiling-refuge-66454.herokuapp.com/images"
+    )
+      .then((response) => response.json())
+      .then((data) => (this.imgArrLinks = data));
+
+    console.log(this.imgArrLinks);
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.modal-box {
+  max-width: 728px;
+  margin: 0 auto;
+}
+
+h1 {
+  margin-bottom: 30px;
+  text-align: center;
+  font-size: 36px;
+  line-height: 42px;
+}
+.gallery {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  row-gap: 20px;
+  justify-content: space-between;
+  align-items: center;
+  &-item {
+    width: 229px;
+  }
+
+  @media (max-width: 687px) {
+    & {
+      justify-content: center;
+
+      &-item {
+        width: 280px;
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
+</style>
